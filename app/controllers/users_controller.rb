674e2851ecfda364fa9234fd7072118b6baf8283
isabell sommerfeld <isi.marie06@gmail.com>
2019-11-26 12:56:02 +0000

    profilepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = current_user
+    @user = User.new(user_params)
+      if @user.save
+        redirect_to user_path(@user)
+      else
+        render 'users/new'
+      end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :role, :first_name, :company_name, :company_photo, :user_photo)
+  end
 end
