fe2c32b80b4b62814f1cf3016bf39b122c3b36e3
annatytko <tytko.anna@gmail.com>
2019-11-26 09:43:59 +0000

    added navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,19 @@
 import "bootstrap";
+
+
+$(function () {
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $(window).resize(function (e) {
+        if ($(window).width() <= 768) {
+            $("#wrapper").removeClass("toggled");
+        } else {
+            $("#wrapper").addClass("toggled");
+        }
+    });
+
+    $("#wrapper").removeClass("toggled");
+});
