2f0ee68097336699f02a935fc04de14ac9d8a4a8
annatytko <tytko.anna@gmail.com>
2019-11-27 21:57:06 +0000

    added swiping without buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,23 +1,104 @@
 import "bootstrap";
 
+// <--navbar-->
 
 $(function () {
-    $("#menu-toggle").click(function (e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-        $("body").toggleClass("overflow-hidden");
-    });
+  $("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+    $("body").toggleClass("overflow-hidden");
+  });
 
-    $(window).resize(function (e) {
-        if ($(window).width() <= 768) {
-            $("#wrapper").removeClass("toggled");
-            $("body").removeClass("overflow-hidden");
-        } else {
-            $("#wrapper").addClass("toggled");
-            $("body").addClass("overflow-hidden");
-        }
-    });
+  $(window).resize(function (e) {
+    if ($(window).width() <= 768) {
+      $("#wrapper").removeClass("toggled");
+      $("body").removeClass("overflow-hidden");
+    } else {
+      $("#wrapper").addClass("toggled");
+      $("body").addClass("overflow-hidden");
+    }
+  });
 
-    $("#wrapper").removeClass("toggled");
-    $("body").removeClass("overflow-hidden");
+  $("#wrapper").removeClass("toggled");
+  $("body").removeClass("overflow-hidden");
 });
+
+// <--swiping-->
+
+$(document).ready(function() {
+
+  var animating = false;
+  var cardsCounter = 0;
+  var numOfCards = 6;
+  var decisionVal = 80;
+  var pullDeltaX = 0;
+  var deg = 0;
+  var $card, $cardReject, $cardLike;
+
+  function pullChange() {
+    animating = true;
+    deg = pullDeltaX / 10;
+    $card.css("transform", "translateX("+ pullDeltaX +"px) rotate("+ deg +"deg)");
+
+    var opacity = pullDeltaX / 100;
+    var rejectOpacity = (opacity >= 0) ? 0 : Math.abs(opacity);
+    var likeOpacity = (opacity <= 0) ? 0 : opacity;
+    $cardReject.css("opacity", rejectOpacity);
+    $cardLike.css("opacity", likeOpacity);
+  };
+
+  function release() {
+
+    if (pullDeltaX >= decisionVal) {
+      $card.addClass("to-right");
+      console.log('action to right');
+    } else if (pullDeltaX <= -decisionVal) {
+      $card.addClass("to-left");
+      console.log('action to left');
+    }
+
+        // LOAD MORE ITEMS TO THE LIST
+        if (Math.abs(pullDeltaX) >= decisionVal) {
+          $card.addClass("inactive");
+
+          setTimeout(function() {
+            $card.addClass("below").removeClass("inactive to-left to-right");
+            cardsCounter++;
+            if (cardsCounter === numOfCards) {
+              cardsCounter = 0;
+              $(".swipe-card").removeClass("below");
+            }
+          }, 300);
+        }
+
+        setTimeout(function() {
+          $card.attr("style", "").removeClass("reset")
+          .find(".swipe-card-choice").attr("style", "");
+
+          pullDeltaX = 0;
+          animating = false;
+        }, 300);
+      };
+
+      $(document).on("mousedown touchstart", ".swipe-card:not(.inactive)", function(e) {
+        if (animating) return;
+
+        $card = $(this);
+        $cardReject = $(".swipe-card-choice.m--reject", $card);
+        $cardLike = $(".swipe-card-choice.m--like", $card);
+        var startX =  e.pageX || e.originalEvent.touches[0].pageX;
+
+        $(document).on("mousemove touchmove", function(e) {
+          var x = e.pageX || e.originalEvent.touches[0].pageX;
+          pullDeltaX = (x - startX);
+          if (!pullDeltaX) return;
+          pullChange();
+        });
+
+        $(document).on("mouseup touchend", function() {
+          $(document).off("mousemove touchmove mouseup touchend");
+            if (!pullDeltaX) return; // prevents from rapid click events
+            release();
+          });
+      });
+    });
