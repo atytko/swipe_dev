17c19f31c833c9082a22a21dcfcd1ab0a439f829
Anna Tytko <tytko.anna@gmail.com>
2019-12-02 18:29:00 +0000

    create redis in heroku

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -2,5 +2,7 @@
 function scrollLastMessageIntoView() {
   const messages = document.querySelectorAll('.message');
   const lastMessage = messages[messages.length - 1];
-  lastMessage.scrollIntoView();
+  if (lastMessage) {
+    lastMessage.scrollIntoView();
+  }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   resources :chat_rooms, only: [ :show, :index ] do
     resources :messages, only: [ :create ]
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
