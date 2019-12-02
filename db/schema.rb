6a853239bc519a748c57392b7a263b0232737a66
isabell sommerfeld <isi.marie06@gmail.com>
2019-12-02 13:04:16 +0000

    created chat, missing frontend tho

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_111421) do
+ActiveRecord::Schema.define(version: 2019_12_02_114241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_111421) do
   create_table "chat_rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "filters", force: :cascade do |t|
